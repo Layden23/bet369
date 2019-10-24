f930657f4e1a2fea0dd96c779d53ef036f7d9bb2
Joel <joelrz.4@hotmail.com>
2019-10-24 15:16:59 +0200

    explorator script

---
+++ Datasets/automatic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 16:07:41 2019
+
+@author: jojoel
+"""
+
+import time,os
+while 1:
+   time.sleep(1800)
+   os.system("python /Users/jojoel/Google\ Drive/Github/bet369/Datasets/explorator.py")
