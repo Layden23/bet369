52e4650f3f735de39f6048bfaa2001936813fe5b
Andreu <andreuoros96@gmail.com>
2019-11-08 13:49:15 +0100

    Flasktrial done

--- Datasets/Flasktrial.py
+++ Datasets/Flasktrial.py
@@ -1,19 +1,11 @@
 import pandas as pd,numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score,cohen_kappa_score,confusion_matrix,recall_score,precision_score
-from sklearn.linear_model import LogisticRegression
-import plotly.express as px
 from datetime import datetime
-import time, random
-import xgboost as xgb
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestClassifier
+import random
 
-
-train = pd.read_csv("C:/Users/andre/Google Drive/Github/bet369/Datasets/train3.csv", header = 0)
+train = pd.read_csv("C:/Users/andre/Google Drive/Github/bet369/Datasets/CSVs/train3.csv", header = 0)
 
 len(train["MatchID"].unique())
 
@@ -44,7 +36,6 @@ for i in train.iloc[:,12:].columns:
     train[i] = pd.to_numeric(train[i])
 #Creating train with matches that don't score in the first half
 train0 = train[(train["Goal"] == 0) & (train["Status"] <= 45)]
-random.seed(69)
 train0 = train0.groupby('MatchID').apply(lambda x: x.sample(1))
 train00 = train[(train["Goal"] == 0) & (train["Status"] <= 45)]
 train00 = train00.groupby('MatchID').apply(lambda x: x.sample(1))
@@ -109,31 +100,12 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,train_size = 0.7)
 rf = RandomForestClassifier(n_estimators = 200)
 rf.fit(X_train, y_train)
 
-# Logistic Regression
-random.seed(s)
-lr = LogisticRegression()
-lr.fit(X_train, y_train)
-
-#XGBTree 
-random.seed(s)
-# cv_params = {'max_depth': [3,5,7], 'min_child_weight': [1,3,5]}
-# ind_params = {'learning_rate': 0.1, 'n_estimators': 1000, 'seed':0, 'subsample': 0.8, 'colsample_bytree': 0.8, 
-#              'objective': 'binary:logistic'}
-# xgb = GridSearchCV(xgb.XGBClassifier(**ind_params), 
-#                             cv_params, 
-#                              scoring = 'accuracy', cv = 5, n_jobs = -1)
-xgb = xgb.XGBClassifier()
-xgb.fit(X_train,y_train)
 
 #Predictions
 rfpreds = rf.predict(X_test)
-lrpreds = lr.predict(X_test)
-xgbpreds = xgb.predict(X_test)
 
 #Probabilities
 rfprobs = rf.predict_proba(X_test)[:,1]
-lrprobs = lr.predict_proba(X_test)[:,1]
-xgbprobs = xgb.predict_proba(X_test)[:,1]
 
 #Raül probs
 Vscore2 = X_test['HostCornersFT']*0.9+X_test['GuestCornersFT']*0.9+X_test['HostYCFT']+X_test['GuestYCFT'] 
@@ -148,53 +120,6 @@ raulpreds = round(raulprobs)
 X_test["raulprobs"] = raulprobs
 X_test.iloc[:,-20:]
 
-results = pd.DataFrame({"Goal": y_test,"rfpreds" : rfpreds,"lrpreds": lrpreds, "xgbpreds": xgbpreds,
-              "rfprobs": rfprobs, "lrprobs": lrprobs, "xgbprobs": xgbprobs, "raulpreds": raulpreds, "raulprobs": raulprobs})
-
-rf_test = results[results["rfprobs"] >= 0.8]
-lr_test = results[results["rfprobs"] >= 0.8]
-xgb_test = results[results["raulprobs"] >= 0.8]
-#n_test = results[results["nprobs"] >= 0.8]
-raul_test = results[results["raulprobs"] >= 0.8]
-pd.DataFrame({"":["0.9 Accuracy","Accuracy","% 0.9 preds","% 0.8 preds","% 0.7 preds"],
-              "rf": [accuracy_score(rf_test["Goal"],rf_test["rfpreds"]), 
-                                 accuracy_score(rfpreds,y_test), 
-                     len(results[results["rfprobs"] >= 0.9])*100/len(X_test),
-                    len(results[results["rfprobs"] >= 0.8])*100/len(X_test),
-                    len(results[results["rfprobs"] >= 0.7])*100/len(X_test)],
-              "lr": [accuracy_score(lr_test["Goal"],lr_test["lrpreds"]), 
-                                 accuracy_score(lrpreds,y_test), 
-                     len(results[results["lrprobs"] >= 0.9])*100/len(X_test),
-                    len(results[results["lrprobs"] >= 0.8])*100/len(X_test),
-                    len(results[results["lrprobs"] >= 0.7])*100/len(X_test)],
-              "xgb": [accuracy_score(xgb_test["Goal"],xgb_test["xgbpreds"]), 
-                                 accuracy_score(xgbpreds,y_test),
-                      len(results[results["xgbprobs"] >= 0.9])*100/len(X_test),
-                     len(results[results["xgbprobs"] >= 0.8])*100/len(X_test),
-                     len(results[results["xgbprobs"] >= 0.7])*100/len(X_test)],
-              "raul": [accuracy_score(raul_test["Goal"],raul_test["raulpreds"]), 
-                                 accuracy_score(raulpreds,y_test), 
-                       len(results[results["raulprobs"] >= 0.9])*100/len(X_test),
-                      len(results[results["raulprobs"] >= 0.8])*100/len(X_test),
-                      len(results[results["raulprobs"] >= 0.7])*100/len(X_test)]
-               })
-    
-print(accuracy_score(results[results["rfprobs"] >= 0.7]["Goal"],results[results["rfprobs"] >= 0.7]["rfpreds"]))
-
-confusion_matrix(xgbpreds,y_test)
-
-confusion_matrix(lrpreds,y_test)
-
-confusion_matrix(rfpreds,y_test)
-
-confusion_matrix(raulpreds,y_test)
-
-print("Metric   "," RF "," LR ","XGB")
-print("Accuracy ", round(accuracy_score(rfpreds,y_test),2), round(accuracy_score(lrpreds,y_test),2), round(accuracy_score(xgbpreds,y_test),2))
-print("Kappa    ",round(cohen_kappa_score(rfpreds,y_test),2),round(cohen_kappa_score(lrpreds,y_test),2),round(cohen_kappa_score(xgbpreds,y_test),2))
-print("Precision",round(precision_score(rfpreds,y_test),2),round(precision_score(lrpreds,y_test),2),round(precision_score(xgbpreds,y_test),2))
-print("Recall   ", round(recall_score(rfpreds,y_test),2),round(recall_score(lrpreds,y_test),2),round(recall_score(xgbpreds,y_test),2))
-
 import os
 os.chdir('C:/Users/andre/Desktop/Ubiqum/Final Project/Datasets/CSVs/'+datetime.now().strftime('%Y-%m-%d')+'/')
 os.getcwd()
@@ -208,7 +133,8 @@ del test["BettingCuoteFT"]
 del test["HostGoalsFT"]
 del test["GuestGoalsFT"]
 test = test.dropna()
-test = test.loc[(test['HostPossessionFT'] != 0) & (test['GuestPossessionFT'] != 0)]
+test['HostPossessionFT'] = test['HostPossessionFT'].replace(0,50)
+test['GuestPossessionFT'] = test['GuestPossessionFT'].replace(0,50)
 test = test.drop(test[test["Status"] == "HT"].index)
 test = test.drop(test[test["Status"] == "NS"].index)
 test = test.drop(test[test["Status"] == "FT"].index)
@@ -228,34 +154,18 @@ test0 = test0[["MatchID","HomeTeamName","GuestTeamName","Status","Goal","Predict
 
 test0.sort_values("Probability", ascending = False)
 
-testj = test0[["MatchID","Probability"]]
-testj.to_json()
+test0 = test0[["MatchID","HomeTeamName", "GuestTeamName","Probability", "Status"]]
 
-import flask
-from flask import request
+testdict = test0.to_dict('r')
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+from flask import Flask
+from flask import jsonify
 
-@app.route('/test')
-def test():
-    return testj
+app = Flask(__name__)
 
-app.run()
+@app.route("/prob")
+def hello():
+    return jsonify(testdict)
 
-# flask depends on this env variable to find the main file
-#export FLASK_APP=flasktrial.py
-
-# now we just need to ask flask to run
-#flask run
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    app.run()
